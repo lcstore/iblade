3d3ccbb031221fd8976399364f4a5e393e27110d
lezo <lcstore@126.com>
2015-10-10 15:15:16 +0800

    update shell

--- src/main/resources/shell/sale.merge.sh
+++ src/main/resources/shell/sale.merge.sh
@@ -9,6 +9,7 @@ function merge_data(){
 	cd $INIT_PATH
 	echo "start to merge data in dir:"$PWD
 	cat *.txt > $OUT_PATH
+	
 }
 v_date=`date +%Y%m%d --date="-0 day"`
 if [[ $# -lt 1 ]]; then
@@ -37,22 +38,10 @@ DEST_DATA_FILE=$PWD"/data/"$v_date"/"$v_site".sale.top."$v_date".data"
 CATE_LOG_PATH=$PWD"/logs/"$v_site".cate."$v_date".out"
 SKU_LOG_PATH=$PWD"/logs/"$v_site".sku."$v_date".out"
 EXEC_CMD="java -Xms256m -Xmx1024m  -Ddest=$CATE_DEST_PATH -cp $JAR_DIR $CATE_CLASS > $CATE_LOG_PATH"
-echo $EXEC_CMD
-eval $EXEC_CMD
-JOB_STATUS=$?
-if [[ $JOB_STATUS -ne 0 ]]; then
-	echo -e "exec cate fail:$JOB_STATUS,ready to exit..."
-	exit 1;
-fi
+
 
 EXEC_CMD="java -Xms256m -Xmx1024m -Dsrc=$CATE_DEST_PATH  -Ddest=$SKU_DEST_PATH -cp $JAR_DIR $SKU_CLASS > $SKU_LOG_PATH"
-echo $EXEC_CMD
-eval $EXEC_CMD
-JOB_STATUS=$?
-if [[ $JOB_STATUS -ne 0 ]]; then
-	echo -e "exec sku fail:$JOB_STATUS,ready to exit..."
-	exit 1;
-fi
+
 echo "" > $DEST_DATA_FILE
 EXEC_CMD="merge_data $SKU_DEST_PATH $DEST_DATA_FILE"
 echo $EXEC_CMD
@@ -64,4 +53,4 @@ if [[ $JOB_STATUS -ne 0 ]]; then
 	exit 1;
 fi
 echo "done v_site:"$v_site",v_date:"$v_date
-ls -l $DEST_DATA_FILE | awk '{ print "file:"$9,",len:"$5 }'
+ls -l $DEST_DATA_FILE
